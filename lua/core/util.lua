e1ff6538653d7a72eaf539f436d793ed7cb965c1
akalankae <akalankae@gmail.com>
2024-01-25 16:36:48 +1100

    feat: add utility functions to insert created date, file name & update time of last modification

--- lua/core/util.lua
+++ lua/core/util.lua
@@ -64,4 +64,43 @@ function Utils.FindRequiredPath(module)
   end
 end
 
+-- Update the last modification time
+-- Look for "Last Modified" in first N rows (8), update when file is re-saved.
+-- NOTE: If you do not use MIN_NUM_LINES, when this file is saved it will update
+-- the string "Last Modified: " on line 82 :)
+function Utils.UpdateLastModifiedTime()
+  local MIN_NUM_LINES = 8 -- skip files that do not have at least this many lines
+  local num_lines = vim.fn.line("$")
+  local bufnr = vim.api.nvim_get_current_buf()
+  if num_lines < MIN_NUM_LINES then
+    return
+  end
+  if vim.api.nvim_buf_get_option(bufnr, "modifiable") then
+    local time = os.date("%d %b %y %I.%M %p")
+    for i = 1, MIN_NUM_LINES do
+      local line = vim.fn.getline(i)
+      local replacement = vim.fn.substitute(line, "Last Modified: \\zs.*", time, "c")
+      if replacement ~= line then
+        vim.fn.setline(i, replacement)
+        vim.notify(vim.fn.expand("<afile>") .. " was modified at " .. time)
+        break
+      end
+    end
+  else
+    vim.notify(vim.fn.expand("<afile>") .. " is not modifiable")
+  end
+end
+
+-- Insert file name & created date
+-- Relevant skeleton file is sourced and file name and created date inserted in to the
+-- designated line when file is created.
+-- WARNING: "modifiable" flag is not checked, so use for above reason only
+function Utils.UpdateSkeletonFile()
+  local time = os.date("%d %b %y")
+  for i = 1, vim.fn.line("$") do
+    vim.fn.setline(i, vim.fn.substitute(vim.fn.getline(i), "Created on: \\zs.*", time, "c"))
+    vim.fn.setline(i, vim.fn.substitute(vim.fn.getline(i), "File name: \\zs.*", vim.fn.expand("<afile>"), "c"))
+  end
+end
+
 return Utils
