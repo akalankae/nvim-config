3bd68d8c404b8abd83ef5e04033506bac7352b09
akalankae <akalankae@gmail.com>
2024-04-06 15:34:04 +1100

    chore: change formatting

--- skeletons/skeleton.c
+++ skeletons/skeleton.c
@@ -1,17 +1,17 @@
-/****************************************************************************************
+/*******************************************************************************
  * File name: <filepath>
  * Author: Akalanka Edirisinghe <akalankae@gmail.com>
  * Created on: <date>
  * Last modified: <date time>
  * Description:
- ***************************************************************************************/
+ ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    // code goes here
-    printf("Hello world\n");
+  // code goes here
+  printf("Hello world\n");
 
-    return (EXIT_SUCCESS);
+  return (EXIT_SUCCESS);
 }
